1c9e9c03ae19ec6dfc50bf82b0075ddd0be31443
Alexander Burgos <axb19c@acu.edu>
2024-10-11 12:28:51 -0500

    Adds Base Menu and Menu Item Row component.

---
+++ app/src/main/java/com/android/example/lunchtray/components/MenuItemRow.kt
@@ -0,0 +1,57 @@
+package com.android.example.lunchtray.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.android.example.lunchtray.model.MenuItem
+
+val padding_medium = 16.dp
+val padding_small = 8.dp
+
+@Composable
+fun MenuItemRow(
+    item: MenuItem,
+    selectedItemName: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        RadioButton(
+            selected = selectedItemName == item.name,
+            onClick = onClick
+        )
+        Column(
+            verticalArrangement = Arrangement.spacedBy(padding_small)
+        ) {
+            Text(
+                text = item.name,
+                style = MaterialTheme.typography.headlineSmall
+            )
+            Text(
+                text = item.description,
+                style = MaterialTheme.typography.bodyLarge
+            )
+            Text(
+                text = item.getFormattedPrice(),
+                style = MaterialTheme.typography.bodyMedium
+            )
+            HorizontalDivider(
+                modifier = Modifier.padding(bottom = padding_medium),
+                thickness = 1.dp
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/android/example/lunchtray/ui/screens/BaseMenu.kt
@@ -0,0 +1,95 @@
+package com.android.example.lunchtray.ui.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.android.example.lunchtray.R
+import com.android.example.lunchtray.components.MenuItemRow
+import com.android.example.lunchtray.model.MenuItem
+
+val padding_medium = 16.dp
+
+@Composable
+fun BaseMenu(
+    options: List<MenuItem>,
+    modifier: Modifier = Modifier,
+    onCancelButtonClicked: () -> Unit = {},
+    onNextButtonClicked: () -> Unit = {},
+    onSelectionChanged: (MenuItem) -> Unit,
+) {
+
+    var selectedItemName by rememberSaveable { mutableStateOf("") }
+
+    Column(modifier = modifier) {
+        options.forEach { item ->
+            val onClick = {
+                selectedItemName = item.name
+                onSelectionChanged(item)
+            }
+            MenuItemRow(
+                item = item,
+                selectedItemName = selectedItemName,
+                onClick = onClick,
+                modifier = Modifier.selectable(
+                    selected = selectedItemName == item.name,
+                    onClick = onClick
+                )
+                    .padding(
+                        start = padding_medium,
+                        end = padding_medium,
+                    )
+            )
+        }
+
+        MenuScreenButtonGroup(
+            selectedItemName = selectedItemName,
+            onCancelButtonClicked = onCancelButtonClicked,
+            onNextButtonClicked = {
+                // Assert not null bc next button is not enabled unless selectedItem is not null.
+                onNextButtonClicked()
+            },
+            modifier = Modifier.fillMaxWidth().padding(padding_medium)
+        )
+    }
+}
+
+
+
+@Composable
+fun MenuScreenButtonGroup(
+    selectedItemName: String,
+    onCancelButtonClicked: () -> Unit,
+    onNextButtonClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.spacedBy(padding_medium)
+    ){
+        OutlinedButton(modifier = Modifier.weight(1f), onClick = onCancelButtonClicked) {
+            Text(stringResource(R.string.cancel).uppercase())
+        }
+        Button(
+            modifier = Modifier.weight(1f),
+            // the button is enabled when the user makes a selection
+            enabled = selectedItemName.isNotEmpty(),
+            onClick = onNextButtonClicked
+        ) {
+            Text(stringResource(R.string.next).uppercase())
+        }
+    }
+}
